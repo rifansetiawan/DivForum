f636e6013bb27988f0f7884271f45c3ac739a0b9
rifansetiawan <37042039+rifansetiawan@users.noreply.github.com>
2019-09-03 18:25:49 +0700

    Create ubah_posting.blade.php

---
+++ div/resources/views/admin_view/ubah_posting.blade.php
@@ -0,0 +1,39 @@
+@extends("admin_view/layout")
+@section("judul","Ubah Balasan")
+@section("isi")
+<form class="form-horizontal" action="{{url('admin/posting/ubah/'.$data->id_posting.'/'.$data->id_thread)}}" method="post">
+			<fieldset>
+				{{ csrf_field() }}
+		@if($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach($errors->all() as $err)
+						<li>{{$err}}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
+
+		
+			<!-- Form Name -->
+			
+
+			<!-- Textarea -->
+			<div class="form-group">
+			  <label class="col-md-12 control-label" for="txt_pesan">Isi Balasan</label>
+			  <div class="col-md-12">                     
+			    <textarea class="form-control" id="txt_pesan" name="txt_pesan">{{$data->isi_posting}}</textarea>
+			  </div>
+			</div>
+
+			<!-- Button -->
+			<div class="form-group">
+			  <label class="col-md-12 control-label" for=""></label>
+			  <div class="col-md-12">
+			    <button id="" name="" class="btn btn-primary">Simpan</button>
+			  </div>
+			</div>
+
+			</fieldset>
+		</form>
+@endsection
