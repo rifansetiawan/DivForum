e4d9fe58f91b8b2d6495eefe2d2be05d921e423d
rifansetiawan <37042039+rifansetiawan@users.noreply.github.com>
2019-09-03 18:25:36 +0700

    Create tambah_kategori.blade.php

---
+++ div/resources/views/admin_view/tambah_kategori.blade.php
@@ -0,0 +1,40 @@
+@extends("admin_view/layout")
+@section("judul","Tambah Kategori")
+@section("isi")
+	<form action="{{url('/admin/kategori/tambah')}}" method="post" enctype="multipart/form-data">
+		{{ csrf_field() }}
+		@if($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach($errors->all() as $err)
+						<li>{{$err}}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
+
+		@if(Session::has("sukses"))
+			<div class="alert alert-success">
+				{{Session::get("sukses")}}
+			</div>
+		@endif
+		<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="txt_nama">Nama</label>  
+  <div class="col-md-4">
+  <input id="txt_nama" name="txt_nama" type="text" placeholder="Nama" class="form-control input-md" value="{{old('txt_nama')}}">
+    
+  </div>
+</div>
+
+
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for=""></label>
+  <div class="col-md-4">
+    <button id="" name="" class="btn btn-primary">Simpan</button>
+  </div>
+</div>
+	</form>
+@endsection
